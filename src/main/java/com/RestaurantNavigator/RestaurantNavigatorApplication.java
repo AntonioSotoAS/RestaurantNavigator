37e23f2d693bf332960b608e01c5e2a527232e65
AntonioSotoAS <arturoantoniomontejosoto@gmail.com>
2023-01-18 16:45:36 -0500

    first project

---
+++ src/main/java/com/RestaurantNavigator/RestaurantNavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.RestaurantNavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantNavigatorApplication.class, args);
+	}
+
+}
